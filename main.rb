a7f9d45f7c20b8fbc98e4a0d31a998bb3643eade
kurokawakazuhiro <k.kurokawa11@gmail.com>
2021-06-24 22:50:20 +0900

    initializeメソッドを使用したため削除

--- main.rb
+++ main.rb
@@ -1,30 +1,21 @@
 class Brave
-  def name=(name)
-    @name = name
+  def initialize
+    @name = "テリー"
+    @hp = 500
+    @offense = 150
+    @defense = 100
   end
 
   def name
     @name
   end
 
-  def hp=(hp)
-    @hp = hp
-  end
-
   def hp
     @hp
   end
 
-  def offense=(offense)
-    @offense = offense
-  end
-
   def offense
     @offense
-  end
-
-  def defense=(defense)
-    @defense = defense
   end
 
   def defense
@@ -33,11 +24,6 @@ class Brave
 end
 
 brave = Brave.new
-
-brave.name = "テリー"
-brave.hp = 500
-brave.offense = 150
-brave.defense = 100
 
 puts <<~TEXT
 NAME：#{brave.name}
